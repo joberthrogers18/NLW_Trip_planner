b830b0a10aaf4b6399331a1bf97d605de1819cd4
Joberth Rogers <joberth.rogers18@gmail.com>
2024-07-16 18:29:38 -0300

    feat: add get trip by id controller

--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -1,13 +1,17 @@
 package com.rocketseat.planner.trip;
 
 import com.rocketseat.planner.participants.ParticipantService;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,13 +25,19 @@ public class TripController {
   private TripRepository tripRepository;
 
   @PostMapping
-  public ResponseEntity<String> createTrip(@RequestBody TripRequestPayload payload) {
+  public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
     Trip newTrip = new Trip(payload);
 
     this.tripRepository.save(newTrip);
     participantService.registerParticipantToEvent(payload.emails_to_invite(), UUID.randomUUID());
 
-    return ResponseEntity.ok().body("Sucesso");
+    return ResponseEntity.ok().body(new TripCreateResponse(newTrip.getId()));
   }
 
+  @GetMapping("/{tripId}")
+  public ResponseEntity<Trip> getTripById(@PathVariable("tripId") UUID id) {
+    Optional<Trip> trip = this.tripRepository.findById(id);
+
+    return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
 }
