e163728d75839a331c9008b5335af7cc28cc86e0
Joberth Rogers <joberth.rogers18@gmail.com>
2024-07-17 14:49:34 -0300

    feat: add controller to update a trip

--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -1,6 +1,8 @@
 package com.rocketseat.planner.trip;
 
 import com.rocketseat.planner.participants.ParticipantService;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,5 +42,24 @@ public class TripController {
     Optional<Trip> trip = this.tripRepository.findById(id);
 
     return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/{tripId}")
+  public ResponseEntity<Trip> updateTrip(@PathVariable("tripId") UUID tripId,
+      @RequestBody TripRequestPayload payload) {
+    Optional<Trip> currentTrip = this.tripRepository.findById(tripId);
+
+    if (currentTrip.isPresent()) {
+      Trip rawTrip = currentTrip.get();
+      rawTrip.setDestination(payload.destination());
+      rawTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+      rawTrip.setStartsAt(
+          LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+
+      this.tripRepository.save(rawTrip);
+      return ResponseEntity.ok(rawTrip);
+    }
+
+    return currentTrip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
   }
 }
