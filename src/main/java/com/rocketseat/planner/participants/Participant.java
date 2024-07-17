45860b2550a231f39d920376050adccfda2c135f
Joberth Rogers <joberth.rogers18@gmail.com>
2024-07-17 15:34:21 -0300

    feat: add participant entity map

---
+++ src/main/java/com/rocketseat/planner/participants/Participant.java
@@ -0,0 +1,43 @@
+package com.rocketseat.planner.participants;
+
+import com.rocketseat.planner.trip.Trip;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "participants")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participant {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column(name = "is_confirmed", nullable = false)
+  private Boolean isConfirmed;
+
+  @ManyToOne
+  @JoinColumn(name = "trip_id", nullable = false)
+  private Trip trip;
+
+}
