e2932fc5918e32a6a4c01f52c5078844b50985b6
Joberth Rogers <joberth.rogers18@gmail.com>
2024-07-19 18:24:57 -0300

    feat: add activity repository

---
+++ src/main/java/com/rocketseat/planner/activity/ActivityRepository.java
@@ -0,0 +1,8 @@
+package com.rocketseat.planner.activity;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+
+}
