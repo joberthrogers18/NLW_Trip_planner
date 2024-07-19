dd90dd20a7dab8e0ca95925c295cf4215c81d9f2
Joberth Rogers <joberth.rogers18@gmail.com>
2024-07-19 18:23:43 -0300

    feat: create the entity to activity table

---
+++ src/main/java/com/rocketseat/planner/activity/Activity.java
@@ -0,0 +1,41 @@
+package com.rocketseat.planner.activity;
+
+import com.rocketseat.planner.trip.Trip;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "activities")
+public class Activity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(name = "occurs_at", nullable = false)
+  private LocalDateTime occursAt;
+
+  @ManyToOne
+  @JoinColumn(name = "trip_id", nullable = false)
+  private Trip trip;
+
+}
