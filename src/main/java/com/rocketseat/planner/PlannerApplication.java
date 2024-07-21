9878e434f8a32e5d174eed5c3d99edbbd86ebc35
Joberth Rogers <joberth.rogers18@gmail.com>
2024-07-21 01:34:56 -0300

    refactor: add todo tasks to do later

--- src/main/java/com/rocketseat/planner/PlannerApplication.java
+++ src/main/java/com/rocketseat/planner/PlannerApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlannerApplication {
 
+	// TODO: add unit tests
+	// TODO: add swagger documentation
+	// TODO: dockerize the application
+	// TODO: use database SQL and Cache
+	// TODO: validate dates trip in request start and end
+	// TODO: validate if dates from activity is between the start and end of trip
+	// TODO: verify if data payload is correct
+	// TODO: abstract the data deal in trip controller to a service
+
 	public static void main(String[] args) {
 		SpringApplication.run(PlannerApplication.class, args);
 	}
